c290af139b9704470f378a19ef145a9048d650b1
GRinvest <grfx@live.ru>
2022-02-11 09:05:35 +0700

    edit timeframe for calculate_kline()

--- main.py
+++ main.py
@@ -39,22 +39,20 @@ async def calculate_kline():
     import pickle
 
     _tasks = []
-    srize = 12
+    srize = 4
     await tasks.find_symbol()
     redis = await aioredis.from_url(REDIS_URL)
     async with ApiSession() as session:
         instance = tasks.AddedKlines(session)
-        async with redis.pipeline(transaction=True) as pipe:
-            time_kline_last, symbols = await pipe.lindex('BTCUSDT:1m', 0).lrange('symbols', 0, -1).execute()
-        if time_kline_last:
-            time_kline_last = pickle.loads(time_kline_last)
-            for _tf in CONFIG['general']['timeframe']:
+        for _tf in CONFIG['general']['timeframe']:
+            async with redis.pipeline(transaction=True) as pipe:
+                time_kline_last, symbols = await pipe.lindex(f'BTCUSDT:{_tf}', 0).lrange('symbols', 0, -1).execute()
+            if time_kline_last:
+                time_kline_last = pickle.loads(time_kline_last)
                 for _s in symbols:
                     symbol = _s.decode("utf-8")
                     _tasks.append(asyncio.create_task(instance.added(symbol, _tf, time_kline_last)))
-        else:
-            srize = 4
-            for _tf in CONFIG['general']['timeframe']:
+            else:
                 for _s in symbols:
                     symbol = _s.decode("utf-8")
                     _tasks.append(asyncio.create_task(instance.new(symbol, _tf)))
