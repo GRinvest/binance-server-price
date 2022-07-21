50e5757815b01286847db6045152ca59a0f50cc8
GRinvest <grfx@live.ru>
2022-07-21 08:20:07 +0700

    add v3.0

--- main.py
+++ main.py
@@ -1,38 +1,20 @@
 import asyncio
 import multiprocessing as mp
 
-from aio_binance.error_handler.error import BinanceException
 from aio_binance.futures.usdt import Client
-from loguru import logger
+
 from config import config
 
 
-async def run_record(e: mp.Event, symbol: list):
+async def run_record(symbol: list):
     from price import record
-    e.wait()
+
     await record.Tasks(symbol).creation()
 
 
-def process_record(e: mp.Event, symbol):
+def process_record(symbol):
     try:
-        asyncio.run(run_record(e, symbol))
-    except KeyboardInterrupt:
-        pass
-
-
-async def run_first(e: mp.Event, symbols_):
-    from price import first
-    try:
-        await first.run(symbols_)
-    except BinanceException as e:
-        logger.error(e)
-    else:
-        e.set()
-
-
-def process_first(e: mp.Event, symbols_):
-    try:
-        asyncio.run(run_first(e, symbols_))
+        asyncio.run(run_record(symbol))
     except KeyboardInterrupt:
         pass
 
@@ -52,16 +34,13 @@ async def run_symbols():
 
 if __name__ == '__main__':
     print('   $$$ Run price program:')
-    event = mp.Event()
     symbols = asyncio.run(run_symbols())
     srize = 10
     try:
-        procs = [
-            mp.Process(target=process_first, args=(event, symbols,))
-        ]
+        procs = []
         srize_symbols = [symbols[i:i + srize] for i in range(0, len(symbols), srize)]
         for symbol_ in srize_symbols:
-            procs.append(mp.Process(target=process_record, args=(event, symbol_, )))
+            procs.append(mp.Process(target=process_record, args=(symbol_, )))
         for proc in procs:
             proc.start()
         for proc in procs:
--- price/record.py
+++ price/record.py
@@ -1,6 +1,6 @@
 import asyncio
 
-import aioredis
+from random import randrange
 import ujson
 from aio_binance.futures.usdt import WsClient
 
@@ -10,32 +10,23 @@ from config import config, redis
 class Tasks:
 
     def __init__(self, symbols: list):
-        self.pipe = None
+        self.conn = None
         self.symbols = symbols
 
     async def event_kline(self, data: dict):
-        k = data['data']['k']
-        alias = ':'.join(['klines', data['data']['ps']])
-        score = k['t']
-        self.pipe.zremrangebyscore(alias, score, score)
-        self.pipe.zadd(alias, {ujson.dumps([
-            score,
-            float(k['o']),
-            float(k['h']),
-            float(k['l']),
-            float(k['c']),
-            float(k['v']),
-            k['T']
-        ]): score})
-        await self.pipe.execute()
+        p = data['data']
+        await self.conn.zadd(':'.join(['price', p['s']]), {ujson.dumps([
+            float(p['p']),
+            float(p['q'])
+        ]): str(p['T']) + str(randrange(100000, 999999, 1))})
 
     async def creation(self):
         ws = WsClient(reply_timeout=160)
         print(f"run: {self.symbols}")
         while True:
             streams = []
-            async with redis.pipeline() as self.pipe:
+            async with redis.client() as self.conn:
                 for symbol in self.symbols:
-                    streams.append(ws.stream_continuous_kline(symbol, 'perpetual', '1m'))
+                    streams.append(ws.stream_agg_trade(symbol))
                 res = await asyncio.gather(*streams)
                 await ws.subscription_streams(res, self.event_kline)
